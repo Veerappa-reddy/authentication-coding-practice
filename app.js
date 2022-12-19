8b2a74ed02a3a3ed9a3e39a438bafad62f7c3d89
veerappa-reddy <veerappa@example.com>
2022-12-19 07:40:36 +0000

    authentication code solution

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const app = express();
+const dbPath = path.join(__dirname, "userData.db");
+
+let db = null;
+const intialiseDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+intialiseDbAndServer();
+
+//register User API
+
+app.post("/register", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = `
+  SELECT 
+    * 
+  FROM 
+    user 
+  WHERE 
+    username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+  INSERT INTO
+    user  (username, name, password, gender, location)
+  VALUES
+    (
+      '${username}',
+      '${name}',
+      '${hashedPassword}',
+      '${gender}',
+      '${location}'  
+    );`;
+    await db.run(createUserQuery);
+    response.send("User Created Successfully");
+  } else {
+    response.status(400);
+    response.send("Username Already Exits");
+  }
+});
+
+module.exports = app;
